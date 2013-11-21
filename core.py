b1bc51088a7c6a76c8422b4bdbb39eb56e81e60f
Shandrinoff.WaD <suguby@gmail.com>
2013-11-21 20:48:03 +0400

    отделил пример игры

--- core.py
+++ core.py
@@ -6,8 +6,7 @@ import random
 from constants import NEAR_RADIUS
 
 from geometry import Point
-from user_interface import BaseSprite, HoneyMeter, GameEngine, SCREENRECT
-from utils import random_number
+from user_interface import BaseSprite, HoneyMeter, SCREENRECT
 
 
 class HoneyHolder():
@@ -303,92 +302,3 @@ class Scene:
         HoneyHolder.honey_speed = honey_speed
 
 
-class WorkerBee(Bee):
-    team = 1
-    all_bees = []
-
-    def is_other_bee_target(self, flower):
-        for bee in WorkerBee.all_bees:
-            if hasattr(bee, 'flower') and bee.flower and bee.flower._id == flower._id:
-                return True
-        return False
-
-    def get_nearest_flower(self):
-        flowers_with_honey = [flower for flower in self.flowers if flower.honey > 0]
-        if not flowers_with_honey:
-            return None
-        nearest_flower = None
-        for flower in flowers_with_honey:
-            if self.is_other_bee_target(flower):
-                continue
-            if nearest_flower is None or self.distance_to(flower) < self.distance_to(nearest_flower):
-                nearest_flower = flower
-        return nearest_flower
-
-    def go_next_flower(self):
-        if self.is_full():
-            self.move_at(self.my_beehive)
-        else:
-            self.flower = self.get_nearest_flower()
-            if self.flower is not None:
-                self.move_at(self.flower)
-            elif self.honey > 0:
-                self.move_at(self.my_beehive)
-            else:
-                i = random_number(0, len(self.flowers) - 1)
-                self.move_at(self.flowers[i])
-
-    def on_born(self):
-        WorkerBee.all_bees.append(self)
-        self.go_next_flower()
-
-    def on_stop_at_flower(self, flower):
-        if flower.honey > 0:
-            self.load_honey_from(flower)
-        else:
-            self.go_next_flower()
-
-    def on_honey_loaded(self):
-        self.go_next_flower()
-
-    def on_stop_at_beehive(self, beehive):
-        self.unload_honey_to(beehive)
-
-    def on_honey_unloaded(self):
-        self.go_next_flower()
-
-
-class GreedyBee(WorkerBee):
-    team = 2
-
-    def get_nearest_flower(self):
-        flowers_with_honey = [flower for flower in self.flowers if flower.honey > 0]
-        if not flowers_with_honey:
-            return None
-        nearest_flower = None
-        max_honey = 0
-        for flower in flowers_with_honey:
-            distance = self.distance_to(flower)
-            if distance > 300:
-                continue
-            if flower.honey > max_honey:
-                nearest_flower = flower
-                max_honey = flower.honey
-            elif flower.honey == max_honey:
-                if nearest_flower is None:
-                    nearest_flower = flower
-                elif distance < self.distance_to(nearest_flower):
-                    nearest_flower = flower
-        if nearest_flower:
-            return nearest_flower
-        return random.choice(flowers_with_honey)
-
-if __name__ == '__main__':
-
-    game = GameEngine("My little garden", resolution=(1000, 500))
-    scene = Scene(beehives_count=2, flowers_count=80, speed=40)
-
-    bees = [WorkerBee() for i in range(10)]
-    bees_2 = [GreedyBee() for i in range(10)]
-
-    game.go(debug=False)
