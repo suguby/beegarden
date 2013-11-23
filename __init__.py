27cceae2515e7bc337e30607f900acdd967e0c20
Shandrinoff.WaD <suguby@gmail.com>
2013-11-23 12:14:06 +0400

    превратил в пакет

---
+++ __init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+from geometry import Point, Vector
+from core import Bee, Flower, BeeHive
+from engine import Scene
+
+__all__ = [Scene, Bee, Flower, BeeHive, Point, Vector]
