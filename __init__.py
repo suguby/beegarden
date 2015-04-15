c2459cdf9d2cf1f3a7a48408be05e65dcf40151f
wad <suguby@gmail.com>
2015-04-15 15:07:50 +0300

    корректировка запуска

--- __init__.py
+++ __init__.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
 """Пакет для игры программистов в пасеку - чья команда пчел быстрее соберет мёд?"""
 
-__all__ = ['geometry', 'core', 'engine']
+__all__ = ['geometry', 'core']
+from .core import Bee, Scene
+from .geometry import Point, Vector
