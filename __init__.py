b140c5196ba73cf55d2e771ce4d9d854f0958430
wad <suguby@gmail.com>
2015-04-17 11:03:05 +0300

    импорты в корень пакета

--- __init__.py
+++ __init__.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 """Пакет для игры программистов в пасеку - чья команда пчел быстрее соберет мёд?"""
 
-__all__ = ['geometry', 'core']
-from .core import Bee, Scene
+__all__ = ['core', 'geometry', 'engine']
+from .core import Bee, Scene, HoneyHolder
 from .geometry import Point, Vector
+from .engine import GameObject
